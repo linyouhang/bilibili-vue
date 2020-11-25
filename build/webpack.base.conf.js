d7785687eb748546220e3f2f2653ca0c743ca993
Stephen-lin30 <754424599@qq.com>
2020-11-25 11:49:58 +0800

    完成大部分功能

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -16,7 +16,8 @@ module.exports = {
   },
   output: {
     path: config.build.assetsRoot,
-    publicPath: process.env.NODE_ENV === 'production' ? config.build.assetsPublicPath : config.dev.assetsPublicPath,
+    publicPath: './',  //资源的公共路径
+    // publicPath: process.env.NODE_ENV === 'production' ? config.build.assetsPublicPath : config.dev.assetsPublicPath,
     filename: '[name].js'
   },
   resolve: {
